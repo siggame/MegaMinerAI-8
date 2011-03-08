613f3673bca3a8f2e6da3b7fa22f0435396af587
Steven Wallace <snw275@mst.edu>
2011-03-08 15:22:30 -0600

    Restored several hand-written files that were lost in the last edit. Did some work on the C# client.

--- codegen/conversions.py
+++ codegen/conversions.py
@@ -8,7 +8,7 @@ import data
 c = {int:'int', str:'char*', float:'float', bool:'int', chr:'char'}
 cpp = {int:'int', str:'string', float:'float', bool:'int', chr:'char'}
 java = {int:'int', str:'String', float:'float', bool:'int', chr:'char'}
-CS = java
+CS = {int:'int', str:'string', float:'float', bool:'int', chr:'char'}
 python = {int:'c_int', str:'c_char_p', float:'c_float', bool:'c_int', chr:'c_char'}
 server = {int:'int', str:'str', float:'float', bool:'int', chr:'char'}
 
@@ -16,5 +16,6 @@ for i in members(data):
     if isinstance(i, structures.Model):
       c[i] = '_' + i.name + '*' 
       java[i] = 'Pointer'
+      CS[i] = 'IntPtr'
       python[i] = 'c_void_p'
       server[i] = 'int'
