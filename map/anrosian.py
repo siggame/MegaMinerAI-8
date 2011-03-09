55724995ffd1ef6761843491b8d1ce219bab516a
Steven Wallace <snw275@mst.edu>
2011-03-09 15:27:34 -0600

    Added a map generator. It works by the oposite of erosion.

---
+++ map/anrosian.py
@@ -0,0 +1,53 @@
+# -*- coding: iso-8859-1 -*-
+import random
+
+class Tile:
+  type = '.'
+  
+def neighbors(size, x, y):
+  result = []
+  for i in (-1, 0), (1, 0), (0, -1), (0, 1):
+    xf = x + i[0]
+    yf = y + i[1]
+    
+    if xf < 0 or xf >= size or yf < 0 or yf >= size:
+      continue
+    else:
+      result.append( (xf, yf) )
+  return result
+
+def groupNeighbors(size, l):
+  result = []
+  for i in l:
+    for j in neighbors(size, i[0], i[1]):
+      result.append(j)
+  return result
+
+def makeMap(size, islands, growth):
+  map = [ [Tile() for i in xrange(size) ] for j in xrange(size) ]
+  for i in xrange(size):
+    for j in xrange(i+1, size):
+      map[i][j] = map[j][i]
+  
+  land = []
+  
+  for i in xrange(islands):
+    x = random.randrange(size)
+    y = random.randrange(size)
+    if map[x][y].type == 'L':
+      continue
+    land.append( (x, y) )
+    map[x][y].type = 'L'
+  
+  print groupNeighbors(size, land)
+  for i in xrange(growth):
+    borders = [i for i in groupNeighbors(size, land) if map[i[0]][i[1]].type == '.' ]
+    next = random.choice(borders)
+    map[next[0]][next[1]].type = 'L'
+    land.append(next)
+  
+  
+  print '\n'.join([''.join([j.type for j in i]) for i in map] )
+  
+makeMap(32,5,100)
+  
