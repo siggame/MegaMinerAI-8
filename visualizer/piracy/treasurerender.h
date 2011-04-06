06c0fc8d629bc2361737ab5029b4dba38582f55e
David Norton <david.k.norton@gmail.com>
2011-04-06 16:11:33 -0500

    Adding some treasurerender stuff. Also, I broke the build. Working on fixing that.

---
+++ visualizer/piracy/treasurerender.h
@@ -0,0 +1,20 @@
+#ifndef TREASURERENDER_H
+#define TRESURERENDER_H
+
+#include "../lib/gocfamily_render.h"
+
+class TreasureRender : public GOCFamily_Render
+{
+public:
+  TreasureRender();
+  ~TreasureRender();
+
+  virtual const GOC_IDType componentID() const;
+
+  void update();
+  virtual void renderAt( 
+      const unsigned int& turn,
+      const unsigned int& frame );
+};
+
+#endif
