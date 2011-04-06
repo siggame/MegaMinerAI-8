debc5351126ab7e035788da98df10debe74ac110
David Norton <david.k.norton@gmail.com>
2011-04-06 16:03:45 -0500

    Made me a treasuredata object for the visualizer, based off of piratedata object.

---
+++ visualizer/piracy/treasuredata.cpp
@@ -0,0 +1,39 @@
+#include "treasuredata.h"
+
+TreasureData::TreasureData()
+{
+}
+
+
+TreasureData::~TreasureData()
+{
+}
+
+const GOC_IDType TreasureData::componentID() const
+{
+  return GOC_IDType( "GOCDataTreasure" );
+}
+
+void TreasureData::update()
+{
+}
+#include <iostream>
+using namespace std;
+
+void TreasureData::parseTreasure( const Game& game, int treasureId )
+{
+  for( int i = 0; i < game.states.size(); i++ )
+  {
+    std::map<int,Treasure>::const_iterator j = game.states[i].treasure.find( treasureId );
+    if( j != game.states[i].treasure.end() )
+    {
+      TreasureDataInfo t;
+      t.x = j->second.x;
+      t.y = j->second.y;
+      t.pirateID = j->second.pirateID;
+      t.amount = j->second.amount;
+      //cout << "Turn: " << i << ", X: " << t.x << ", Y: " << t.y << endl;
+      m_timeline.add( i, 0, t );
+    }
+  }
+}
---
+++ visualizer/piracy/treasuredata.h
@@ -0,0 +1,27 @@
+#ifndef TREASUREDATA_H
+#define TREASUREDATA_H
+
+#include "../lib/gocfamily_data.h"
+#include "../lib/parser/parser.h"
+
+struct TreasDataInfo
+{
+  int x;
+  int y;
+  int pirateID; //Pirate carrying treasure, 0 if not carried (lying on ground)
+  int amount; 
+};
+
+class TreasureData : public GOCFamily_Data<PirateDataInfo>
+{
+public:
+  TreasureData();
+  ~TreasureData();
+
+  void update();
+  const GOC_IDType componentID() const;
+  void parseTreasure( const Game& game, int treasureId );
+private:
+};
+
+#endif
