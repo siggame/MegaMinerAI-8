14fbe8794f44c61e0e50bf1eb9ed3729c7a8af33
Steven <sawwx7@mst.edu>
2011-04-13 15:40:22 -0500

    Fixed a bug where merchant ships couldn't drop off their gold

--- server/game_app/objects.py
+++ server/game_app/objects.py
@@ -579,7 +579,7 @@ class Ship(Unit):
                 #True if we find a port at desired location
                 portTile = True
                 #If the port does not belong to you, throw an error
-                if j.owner != self.game.playerID:
+                if j.owner != self.game.playerID and self.game.playerID != 2  and self.game.playerID != 3:
                   return "You cannot move into enemy ports!"
           #If the player is simply trying to move a ship onto land
           if portTile == False:
