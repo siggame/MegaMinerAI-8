8d4cc4f76900cf1cf087b94dd6edc959530442c1
TheAkss999 Laptop <aksfkb@mst.com>
2011-04-13 22:22:34 -0500

    better interface for objectses

--- visualizer/lib/objectmanager/objectmanager.h
+++ visualizer/lib/objectmanager/objectmanager.h
@@ -12,18 +12,30 @@ class ObjectManager : public Singleton<ObjectManager>
 public:
     void setSize(const unsigned int & turns, const unsigned int & frames)
     {
-	//get()->m_objects.
+	if (!Singleton<ObjectManager>::isInit())
+	    return;
+	Singleton<ObjectManager>::get()->m_objects.setSize(turns,frames);
     }
 
     unsigned int frames()
     {
+	if (!Singleton<ObjectManager>::isInit())
+	    return 0;
+	return Singleton<ObjectManager>::get()->m_objects.getFrames();
     }
 
-    unsigned int turns();
+    unsigned int turns()
+    {
+	if (!Singleton<ObjectManager>::isInit())
+	    return 0;
+	return Singleton<ObjectManager>::get()->m_objects.getTurns();
+    }
 
     static bool reg(const ObjIdType & id, GameObject * obj, const unsigned int & turn, const unsigned int & frame);
     static bool del(const ObjIdType & id, const unsigned int & turn, const unsigned int & frame);
     static GameObject * get(const ObjIdType & id, const unsigned int & turn, const unsigned int & frame);
+
+    static bool exists(const ObjIdType & id, const unsigned int & turn, const unsigned int & frame);
 
 private:
     LookupTable<ObjIdType,GameObject*> m_objects;
