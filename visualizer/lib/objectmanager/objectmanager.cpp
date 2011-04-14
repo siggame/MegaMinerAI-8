ea3a9dff0cc7bdd2f242093cb95508d97f026da8
TheAkss999 Laptop <aksfkb@mst.com>
2011-04-13 22:54:50 -0500

    better crap for timelining?

---
+++ visualizer/lib/objectmanager/objectmanager.cpp
@@ -0,0 +1,27 @@
+#include "objectmanager.h"
+
+bool ObjectManager::reg(const ObjIdType & id, GameObject * object , const unsigned int & turn, const unsigned int & frame)
+{
+    GameObject * temp = object;
+
+
+    return true;
+}
+
+bool ObjectManager::del(const ObjIdType & id, const unsigned int & turn, const unsigned int & frame )
+{
+   // if ()
+
+    return true;
+}
+
+
+GameObject * ObjectManager::get(const ObjIdType & id, const unsigned int & turn, const unsigned int & frame )
+{
+
+
+
+}
+
+
+
