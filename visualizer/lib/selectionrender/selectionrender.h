01b53048198deb6619529ad5a9d80ce311b3e74e
David Norton <dkn2n4@mail.mst.edu>
2011-04-14 23:36:16 -0500

    Starting SelectionRender singleton object.

---
+++ visualizer/lib/selectionrender/selectionrender.h
@@ -0,0 +1,9 @@
+#ifndef SELECTIONRENDER_H
+#define SELECTIONRENDER_H
+
+class SelectionRender : public GOCFamily_Render; Singleton <SelectionRender>
+{
+	void setSelectionBox(int, int, int, int) //X1, Y1, X2, Y2
+}
+
+#endif
