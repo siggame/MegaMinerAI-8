2d12356fa6cd9a82ef24cff44c5a08c1f6069edf
Steven Wallace <snw275@mst.edu>
2011-04-16 17:25:15 -0500

    Made a new dict.

---
+++ server/game_app/objectHolder.py
@@ -0,0 +1,12 @@
+import objects
+
+class ObjectHolder(dict):
+  def __init__(self, *args, **kwargs):
+    dict.__init__(self, *args, **kwargs)
+    lists = {}
+    lists
+
+  def __setitem__(self, key, value):
+    dict.__setitem__(self, key, value)
+
+
