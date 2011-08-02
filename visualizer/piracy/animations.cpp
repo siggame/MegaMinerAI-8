cb03f6f17111ebd96e21c27e415237ca1111dc40
Jacob Gardner <jacob.v.gardner@gmail.com>
2011-08-01 22:32:54 -0500

    Animation notes to think about.

--- visualizer/piracy/animations.cpp
+++ visualizer/piracy/animations.cpp
@@ -12,7 +12,26 @@ namespace visualizer
 
   void LeftAnim::animate( float t, AnimData *d )
   {
+    // An interesting idea for ship animations is letting the ship animations 
+    // sort of draw out at the end past their controlled duration time. 
+    // When these bleed over into another move animation, the ship won't appear
+    // to slow down, but just smoothly continue on.  It can't be too long, or 
+    // ships will collide with each other on turns. 
     PirateData& data = ((PirateData&)*d);
+    // if( m_ships > 0 )
+    /*
+    {
+      interp_sigmoid( blah, m_startTime, m_endTime, t );
+    } 
+    else
+    {
+      interp_lin( m_startTime, m_endTime, t );
+    }
+    */
+    // Need interpolation helper functions
+    //interpolate( m_startTime, m_endTime, t );
+    //interpolate_exp( param, m_startTime, m_endTime, t );
+    //interpolate_sigmoid( param, m_startTime, m_endTime, t );
   }
 
   void RightAnim::animate( float t, AnimData *d )
