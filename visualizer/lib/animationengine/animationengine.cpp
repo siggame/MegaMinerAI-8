4e96b4b31d234341e85dda1868d0aacdbeb5c819
Jacob Gardner <jacob.v.gardner@gmail.com>
2011-08-10 21:23:21 -0500

    Time calculations may be done.  Still need to test.

--- visualizer/lib/animationengine/animationengine.cpp
+++ visualizer/lib/animationengine/animationengine.cpp
@@ -29,6 +29,8 @@ namespace visualizer
 
   void _AnimationEngine::buildAnimations( Frame& frame )
   {
+    // @NOTE: Can we simplify this looping any?  Not a huge issue since it's internal
+    // if this were on the piracy side, I'd be a little more worried
     for
       ( 
       std::list<SmartPointer<Animatable> >::iterator i = frame.getAnimations().begin();
@@ -37,6 +39,9 @@ namespace visualizer
       )
     {
       IAnimator& anim = (*i)->getAnimationSequence( 0 );
+      float totalDuration = 0;
+      float extraTime = 0;
+      float exT;
       for
         (
         IAnimator::Iiterator& j = *anim.begin();
@@ -44,7 +49,22 @@ namespace visualizer
         j++
         )
       {
+        totalDuration += j->controlDuration();
+        exT = j->totalDuration() - j->controlDuration();
+        extraTime = extraTime > exT ? extraTime : exT;
+      }
 
+      float fullTime = totalDuration + extraTime;
+      float start = 0;
+      for
+        (
+        IAnimator::Iiterator& j = *anim.begin();
+        j.done();
+        j++
+        )
+      {
+        j->startTime = start;
+        start = j->endTime = j->startTime + (j->controlDuration()/fullTime);
       }
     }
   }
