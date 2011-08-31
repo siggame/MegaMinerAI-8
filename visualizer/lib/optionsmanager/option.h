b68aebf86368458efcfb9318614cee0cb25c39d1
Jacob Gardner <jacob.v.gardner@gmail.com>
2011-08-31 14:25:57 -0500

    Some formatting things

--- visualizer/lib/optionsmanager/option.h
+++ visualizer/lib/optionsmanager/option.h
@@ -4,38 +4,38 @@
 namespace visualizer
 {
 
-enum OptionType
-{
-  OT_NONE,
-  OT_INT,
-  OT_FLOAT,
-  OT_STRING,
-  OT_BOOL
-};
+  enum OptionType
+  {
+    OT_NONE,
+    OT_INT,
+    OT_FLOAT,
+    OT_STRING,
+    OT_BOOL
+  };
 
-class OptionBase
-{
-  public:
-    OptionBase(const OptionType & ot):m_type(ot){}
-    OptionType type(){return m_type;}
+  class OptionBase
+  {
+    public:
+      OptionBase(const OptionType & ot):m_type(ot){}
+      OptionType type(){return m_type;}
 
-  protected:
-    OptionType m_type;           //!< Member variable "m_type"
-  private:
+    protected:
+      OptionType m_type;           //!< Member variable "m_type"
+    private:
 
-};
+  };
 
-template<class T, OptionType ot>
-class Option : public OptionBase
-{
-  public:
-    Option(const T & data):OptionBase(ot),m_data(data){}
-    Option():OptionBase(ot){}
-    const T& get(){return m_data;}
-    void set(const T & data){m_data = data;}
-  protected:
-    T m_data;
-};
+  template<class T, OptionType ot>
+  class Option : public OptionBase
+  {
+    public:
+      Option(const T & data):OptionBase(ot),m_data(data){}
+      Option():OptionBase(ot){}
+      const T& get(){return m_data;}
+      void set(const T & data){m_data = data;}
+    protected:
+      T m_data;
+  };
 
 } // visualizer
 
