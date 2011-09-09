8ddd0bb863ede2212814b3304016010877ae8646
Jacob Gardner <jacob.v.gardner@gmail.com>
2011-09-09 01:42:05 -0500

    Animations

---
+++ visualizer/bloomless/animations.cpp
@@ -0,0 +1,53 @@
+#include "animations.h"
+#include <iostream>
+using namespace std;
+
+namespace visualizer
+{
+
+  void StartAnim::animate( const float& t, AnimData *d )
+  {
+    
+  }
+
+  void DrawGround::animate( const float& t, AnimData *d )
+  {
+    IRenderer& r = *m_dirt->Renderer;
+    r.setColor( 1, 1, 1, 1 );
+
+    r.drawTexturedQuad( 0, 0, 30, 30, "dirt" );
+  }
+
+  void DrawAnim::animate( const float& t, AnimData *d )
+  {
+    IRenderer& r = *m_plant->Renderer;
+    plant& p = *m_plant;
+
+    r.setColor( 1, 1, 1, 1 );
+
+
+    if( p.flower )
+    {
+      r.drawTexturedQuad( p.x, p.y, 1, 1, "flower" );
+    }
+    else
+    {
+      r.drawTexturedQuad( p.x, p.y, 1, 1, "plant" );
+    }
+
+#if 0
+    if( p.leaf )
+    {
+      r.drawTexturedQuad( p.x + 0.5, p.y, 0.5, 0.5, "leaf" );
+    }
+
+    if( p.root )
+    {
+      r.drawTexturedQuad( p.x, p.y+0.5, 0.5, 0.5, "root" );
+    }
+#endif
+
+  }
+
+} // visualizer 
+
