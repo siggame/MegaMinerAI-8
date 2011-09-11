32f42e63606146e7b606ab760ea0a1701a52868d
Jacob Gardner <jacob.v.gardner@gmail.com>
2011-09-10 22:25:40 -0500

    Comments everywhere!

--- visualizer/bloom/bloom.cpp
+++ visualizer/bloom/bloom.cpp
@@ -1,22 +1,27 @@
 #include "bloom.h"
-#include "animations.h"
+// Include the animations we just made
+#include "animations.h"   
+
 #include "plant.h"
+// We're also going to include frame.h because 
+// We have to store all of our animatables in an animation frame
 #include "frame.h"
+
 
 namespace visualizer
 {
   Bloom::Bloom() // Constructor
   {
     m_game = 0;  // Don't setup the game yet.
-  }
+  } // Bloom::Bloom()
 
   Bloom::~Bloom() // Deconstructor
   {
     delete m_game; // Cleanup once we're done
-  }
+  } // Bloom::~Bloom()
 
   LogRegex Bloom::logFileInfo() 
-      // Function tells the visualizer core what gamelogs we can handle
+      // Function tells the visualizer core what gamelogs we are responsible for
   {
     LogRegex lr; // Structure which holds regex to determine gamelog 
     // ownership
@@ -24,25 +29,47 @@ namespace visualizer
     lr.startSize = 500; // How many characters to read before giving up.
 
     return lr;
-  }
+  } // Bloom::logFileInfo()
 
   void Bloom::loadGamelog( std::string gamelog )
   {
+    // This will be invoked auotmatically by the core visualizer when
+    // it determines this plugin IS the one responsible for the the 
+    // gamelog it just received. 
+
+    // We'll just use this to prove that it's loading bloom when it
+    // should
     cout << "Load Bloom Gamelog" << endl;
+
+    // Let's just make sure that we don't have memory leaks.
     delete m_game; 
+
+    // Allocates the memory we need for the new gamelog
     m_game = new Game;
 
+    // So this is really important.  This loads the gamelog into 
+    // memory when we call it.
     if( !parseFile( *m_game, (char*)gamelog.c_str() ) ) // Not safe
     {
+      // It didn't load correctly.  Cleanup everything
       delete m_game;
-         THROW
+      // This is special macros for throwing exceptions.  
+      // It's good to use exceptions whenever you can so we know
+      // exactly when and where our code is failing.  It will
+      // immediately exit and print the error
+      THROW
         (
           GameException, "Cannot Load The Gamelog."
         );
-    }
+    } // Bloom::loadGamelog()
 
+    // We need to keep track of all of the frames that are in this game
     size_t frameNum = 0;
 
+    // So the next thing we need to do is translate all the parsed
+    // units into our animatables.   
+
+    // We do this by looping through all the states in the game.
     for
       (
       std::vector<GameState>::iterator state = m_game->states.begin();
@@ -50,7 +77,11 @@ namespace visualizer
       state++
       )
     {
+      // First thing we need to do is make a new Frame and we'll just 
+      // call it turn
       Frame turn;
+      // For each game state, we have to loop through all the plants
+      // and add them to the stack. 
       for
         (
         std::vector<Plant>::iterator i = state->plants.begin();
@@ -58,36 +89,52 @@ namespace visualizer
         i++
         )
       {
+        // We'll create a new plant animatable
         plant *p = new plant( renderer );
 
-        p->addKeyFrame( new StartAnim() );
+        // We want to add the StartAnim to the plant  first
+        p->addKeyFrame( new StartAnim );
 
+        // We just want to copy all the attributes we need from the parser
+        p->ownerID = i->ownerID;
         p->x = i->x;
         p->y = i->y;
         p->root = i->root;
-        p->flower = i->flower;
         p->leaf = i->leaf;
+        p->flower = i->flower;
         p->health = i->health;
-        p->ownerID = i->ownerID;
 
+        // Then we want to add the draw animation to the plant
         p->addKeyFrame( new DrawPlant( p ) );
 
+        // Once we're done with the animatable, we have to add it to 
+        // the frame.
         turn.addAnimatable( p );
+
       }
 
+      // At this point all the animatables have been put into the frame, 
+      // so we're done with the frame.  We can add the frame to the 
+      // master timeline.
       addFrame( turn );
-
+      // Since this frame is added, we'll increment the frame count:
       frameNum++;
 
     }
 
+    // To let our timeline in the main visualizer know how many frames
+    // we've processed, we have to give them the frame count:
     timeManager->setNumTurns( frameNum );
 
+    // Everything's ready to be played at this point, so we want to 
+    // let the animation engine we're ready to go
     animationEngine->registerFrameContainer( this );
 
+    // Now let's just go ahead and start playing
     timeManager->play();
 
-  }
-}
+  } // Bloom::loadGamelog()
+
+} // visualizer
 
 Q_EXPORT_PLUGIN2( bloom, visualizer::Bloom );
--- visualizer/bloom/bloom.pro
+++ visualizer/bloom/bloom.pro
@@ -1,21 +1,30 @@
-INCLUDEPATH += ../interfaces \
-  ../parser \
-  ../parser/sexp \
-  ../common/
-
-DEPENDPATH += ../common/
-
+INCLUDEPATH += ../interfaces \  # INCLUDEPATH tells the compiler where to find our
+  ../parser \                   # header files for this project.
+  ../parser/sexp \              # interfaces is the code to join the plugin to the core
+  ../common/                    # parser is self-explanatory. Common contains code common 
+                                # to the visualizer core and plugins
+DEPENDPATH += ../common/        # DEPENDPATH are the files that we want to check in addition
+                                # to those that we are writing now for changes to see
+                                # if we need to recompile
 win32:LIBS += ./parser/sexp/sexpXP.a
-else:LIBS += ./parser/sexp/sexp.a
-
-TEMPLATE = lib 
-TARGET = bloom
-SOURCES += *.cpp \ 
+else:LIBS += ./parser/sexp/sexp.a # This is just hooking bloom up to the parser
+TEMPLATE = lib      # Tells the compiler that this is a library and not a standalone program
+TARGET = bloom      # Tells the compiler that were naming this plugin "bloom"
+SOURCES += *.cpp \  # What are the cpp files for this plugin
   ./parser/*.cpp
+  
+HEADERS += *.h  # What are the header files for this plugin
 
-HEADERS += *.h
+# These tell the compiler that were going to compile and link with profiling 
+# and debugging symbols.  These help us out when the visualizer is going really slow
+# or something is going really wrong.
+QMAKE_CFLAGS_DEBUG += -pg    
+QMAKE_CXXFLAGS_DEBUG += -pg
+QMAKE_LFLAGS_DEBUG += -pg
+CONFIG += debug plugin    # Tells the compiler that this is going to be a debug build
+                          # and a plugin
 
-CONFIG += debug plugin 
+debug:DEFINES += __DEBUG__  # This constant will be available when were in a debug build.
+DESTDIR = ../plugins/       # Where to put the plugin on a successful build.
 
-debug:DEFINES += __DEBUG__
-DESTDIR = ../plugins/ 
+
