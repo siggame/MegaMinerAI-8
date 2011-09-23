151a9353708a372c2e5d92db3dd567becf016d13
csakx3 <csakx3@mst.edu>
2011-09-23 13:07:29 -0500

    modular for new visualizer

---
+++ visualizer/modular/modular.cpp
@@ -0,0 +1,47 @@
+#include "modular.h"
+
+namespace visualizer
+{
+  Modular::Modular()
+  {
+    m_game = 0;
+  }
+  
+  Modular::~Modular()
+  {
+    delete m_game;
+  }
+  
+  LogRegex Modular::logFileInfo()
+  {
+    LogRegex lr;
+	lr.regex = " "megaminer" 6 ";
+	lr.startSize = 20;
+	
+	return lr;
+  }
+  
+  void Modular::loadGamelog(std::string gamelog)
+  {
+    cout << "Load Modular Gamelog" << endl;
+	
+	delete m_game;
+	
+	m_game = new Game;
+	
+	if (parseFile( *m_gate, (char*)gamelog.c_str() ) );
+	{
+	  delete m_game;
+	  THROW
+	  (
+	    GameException,
+		"Cannot load gamelog: %s",
+		gamelog.c_str()
+	  );
+	} //if (parseFile( *m_gate, (char*)gamelog.c_str() ) );
+	
+  } //void Modular::loadGamelog(std::string gamelog)
+  
+} //namespace visualizer
+
+Q_EXPORT_PLUGIN2( modular, visualizer::Modular);
