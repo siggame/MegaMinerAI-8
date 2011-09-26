7fb52691630d46edaeb1fc11d3ed5ca855389d18
Goldman <brian.goldman@mst.edu>
2011-09-26 15:13:19 -0500

    Now tracking the server so that we can add game logic

---
+++ server/game_app/objects.py
@@ -0,0 +1,119 @@
+class Mappable:
+  def __init__(self, game, id, x, y):
+    self.game = game
+    self.id = id
+    self.x = x
+    self.y = y
+
+  def toList(self):
+    value = [
+      self.id,
+      self.x,
+      self.y,
+      ]
+    return value
+
+  def nextTurn(self):
+    pass
+
+
+
+class Base(Mappable):
+  def __init__(self, game, id, x, y, owner):
+    self.game = game
+    self.id = id
+    self.x = x
+    self.y = y
+    self.owner = owner
+
+  def toList(self):
+    value = [
+      self.id,
+      self.x,
+      self.y,
+      self.owner,
+      ]
+    return value
+
+  def nextTurn(self):
+    pass
+
+  def Spawn(self, Level):
+    pass
+
+
+
+class Player:
+  def __init__(self, game, id, playerName, byteDollars, cycles):
+    self.game = game
+    self.id = id
+    self.playerName = playerName
+    self.byteDollars = byteDollars
+    self.cycles = cycles
+
+  def toList(self):
+    value = [
+      self.id,
+      self.playerName,
+      self.byteDollars,
+      self.cycles,
+      ]
+    return value
+
+  def nextTurn(self):
+    pass
+
+  def talk(self, message):
+    pass
+
+
+
+class Tile(Mappable):
+  def __init__(self, game, id, x, y, owner):
+    self.game = game
+    self.id = id
+    self.x = x
+    self.y = y
+    self.owner = owner
+
+  def toList(self):
+    value = [
+      self.id,
+      self.x,
+      self.y,
+      self.owner,
+      ]
+    return value
+
+  def nextTurn(self):
+    pass
+
+
+
+class Virus(Mappable):
+  def __init__(self, game, id, x, y, owner, level):
+    self.game = game
+    self.id = id
+    self.x = x
+    self.y = y
+    self.owner = owner
+    self.level = level
+
+  def toList(self):
+    value = [
+      self.id,
+      self.x,
+      self.y,
+      self.owner,
+      self.level,
+      ]
+    return value
+
+  def nextTurn(self):
+    pass
+
+  def move(self, x, y):
+    pass
+
+
+
