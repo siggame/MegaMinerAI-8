9c22ad7b3fea7ad3e59367a0f5fb21bd35f8a8d1
Jacob Gardner <jacob.v.gardner@gmail.com>
2011-09-26 21:47:38 -0500

    Add build botnet script

--- visualizer/bloomless/bloom.cpp
+++ visualizer/bloomless/bloom.cpp
@@ -5,16 +5,15 @@
 
 namespace visualizer
 {
-
   Bloom::Bloom()
   {
     m_game = 0;
-  } // Bloom::Bloom()
+  }
 
   Bloom::~Bloom()
   {
     delete m_game;
-  } // Bloom::~Bloom()
+  }
 
   LogRegex Bloom::logFileInfo()
   {
@@ -23,42 +22,40 @@ namespace visualizer
     lr.startSize = 500;
 
     return lr;
+  }
 
-  } // Bloom::logFileInfo()
-  
   void Bloom::loadGamelog( std::string gamelog )
   {
-
     cout << "Load Bloom Gamelog" << endl;
-    unsigned int frameNum = 0;
+
     delete m_game;
+
     m_game = new Game;
+
     if( !parseFile( *m_game, (char*)gamelog.c_str() ) )
     {
       delete m_game;
       THROW
         (
-          GameException, "Cannot Load The Gamelog."
+        GameException, 
+        "Cannot load gamelog: %s", 
+        gamelog.c_str()
         );
     }
 
-    resourceManager->loadResourceFile( "./plugins/bloom/textures.r" );
-    options->setInt( "mapSize", 20 );
+    resourceManager->loadResourceFile( "./plugins/piracy/textures.r" );
 
-    dirt *tD = new dirt;
-    tD->Renderer = renderer;
-    tD->addKeyFrame( new DrawGround( tD ) );
+    size_t frameNum = 0;
 
-    SmartPointer<Animatable> d( tD );
     for
-      (
+      ( 
       std::vector<GameState>::iterator state = m_game->states.begin();
       state != m_game->states.end();
       state++
       )
     {
       Frame turn;
-      turn.addAnimatable( d );
+
       for
         (
         std::vector<Plant>::iterator i = state->plants.begin();
@@ -66,10 +63,11 @@ namespace visualizer
         i++
         )
       {
-        plant *p = new plant;
-        p->addKeyFrame( new StartAnim( p ) );
+        plant *p = new plant( renderer );
 
-        p->objectID = i->objectID;
+        p->addKeyFrame( new StartAnim );
+
+        p->ownerID = i->ownerID;
         p->x = i->x;
         p->y = i->y;
         p->root = i->root;
@@ -77,28 +75,25 @@ namespace visualizer
         p->flower = i->flower;
         p->health = i->health;
 
+        p->addKeyFrame( new DrawPlant( p ) );
 
-        p->addKeyFrame( new DrawAnim( p ) );
         turn.addAnimatable( p );
-
       }
-      // turn.addAnimatable( score and stuff );
+
       addFrame( turn );
-      frameNum++;
-      timeManager->setNumTurns( frameNum );
-      timeManager->updateProgress( (float)frameNum/m_game->states.size() );
 
-      if( frameNum <= 1 )
-      {
-        animationEngine->registerFrameContainer( this );
-        timeManager->play();
-      }
+      frameNum++;
 
     }
 
-  } // Bloom::loadGamelog()
+    timeManager->setNumTurns( frameNum );
+
+    animationEngine->registerFrameContainer( this );
+
+    timeManager->play();
+
+  }
 
 } // visualizer
 
 Q_EXPORT_PLUGIN2( bloom, visualizer::Bloom );
-
--- visualizer/bloomless/bloom.pro
+++ visualizer/bloomless/bloom.pro
@@ -1,22 +1,20 @@
 INCLUDEPATH += ../interfaces \
-  ../parser \
-  ../parser/sexp \
-  ../common/
-DEPENDPATH += ../common/
+ ../parser \
+ ../parser/sexp \
+ ../common/ 
+
+DEPENDPATH += ../common/ 
+
 win32:LIBS += ./parser/sexp/sexpXP.a
 else:LIBS += ./parser/sexp/sexp.a
-MOC = moc
+
 TEMPLATE = lib
-TARGET = bloomless
-SOURCES += *.cpp \
+TARGET =  bloom 
+SOURCES = *.cpp \
   ./parser/*.cpp
-  
-HEADERS += bloom.h
+HEADERS += *.h
 
-QMAKE_CFLAGS_DEBUG += -pg
-QMAKE_CXXFLAGS_DEBUG += -pg
-QMAKE_LFLAGS_DEBUG += -pg
 CONFIG += debug plugin
-
 debug:DEFINES += __DEBUG__
+
 DESTDIR = ../plugins/
