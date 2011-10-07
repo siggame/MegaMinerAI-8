804f9c2bced40ef91a76b1ddbbab1edddaf3c24b
Jacob Gardner <jacob.v.gardner@gmail.com>
2011-10-07 17:06:30 -0500

    BotNet compiles

--- visualizer/botnet/botnet.cpp
+++ visualizer/botnet/botnet.cpp
@@ -1,19 +1,17 @@
-#include "bloom.h"
-#include "plant.h"
 #include "frame.h"
-#include "animations.h"
+#include "botnet.h"
 
 namespace visualizer
 {
   BotNet::BotNet()
   {
     m_game = 0;
-  }
+  } // BotNet::BotNet()
 
   BotNet::~BotNet()
   {
     delete m_game;
-  }
+  } // BotNet::~BotNet()
 
   LogRegex BotNet::logFileInfo()
   {
@@ -22,78 +20,13 @@ namespace visualizer
     lr.startSize = 500;
 
     return lr;
-  }
+  } // BotNet::logFileInfo()
 
   void BotNet::loadGamelog( std::string gamelog )
   {
     cout << "Load Bloom Gamelog" << endl;
-
-    delete m_game;
-
-    m_game = new Game;
-
-    if( !parseFile( *m_game, (char*)gamelog.c_str() ) )
-    {
-      delete m_game;
-      THROW
-        (
-        GameException, 
-        "Cannot load gamelog: %s", 
-        gamelog.c_str()
-        );
-    }
-
-    resourceManager->loadResourceFile( "./plugins/piracy/textures.r" );
-
-    size_t frameNum = 0;
-
-    for
-      ( 
-      std::vector<GameState>::iterator state = m_game->states.begin();
-      state != m_game->states.end();
-      state++
-      )
-    {
-      Frame turn;
-
-      for
-        (
-        std::vector<Plant>::iterator i = state->plants.begin();
-        i != state->plants.end();
-        i++
-        )
-      {
-        plant *p = new plant( renderer );
-
-        p->addKeyFrame( new StartAnim );
-
-        p->ownerID = i->ownerID;
-        p->x = i->x;
-        p->y = i->y;
-        p->root = i->root;
-        p->leaf = i->leaf;
-        p->flower = i->flower;
-        p->health = i->health;
-
-        p->addKeyFrame( new DrawPlant( p ) );
-
-        turn.addAnimatable( p );
-      }
-
-      addFrame( turn );
-
-      frameNum++;
-
-    }
-
-    timeManager->setNumTurns( frameNum );
-
-    animationEngine->registerFrameContainer( this );
-
-    timeManager->play();
-
-  }
+  } // BotNet::loadGamelog() 
 
 } // visualizer
 
-Q_EXPORT_PLUGIN2( botnet, visualizer::Botnet );
+Q_EXPORT_PLUGIN2( botnet, visualizer::BotNet );
