4d1fcc300812ad7a3fcf9acbc1078b94d847dff1
Dale Matthews <dem7w2@mst.edu>
2011-10-07 21:29:01 -0500

    made a drawTile in animations.cpp

--- visualizer/botnet/animations.cpp
+++ visualizer/botnet/animations.cpp
@@ -17,6 +17,21 @@ namespace visualizer
     
     v.renderer().drawQuad( v.x, v.y, 1, 1);
   }
+  void DrawTile::animate( const float& t, AnimData *d )
+  {
+    tile &t = *m_tile;
+
+    if( t.owner == 0)
+      t.renderer().setColor( Color(0.5, 0, 0) ); 
+    else if ( t.owner == 1)
+      t.renderer().setColor( Color( , , ) );
+    else if ( t.owner == 2)
+      t.renderer().setColor( Color( , , ) );
+    else if ( t.owner == 3)
+      t.renderer().setColor( Color( , , ) );
+    
+    t.renderer().drawQuad( t.x, t.y, 1, 1);
+  }
 } // visualizer
 
 
