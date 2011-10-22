a610b547e090c611a4ad4269659b41d71403522b
Jacob Fischer <jtf3m8@mst.edu>
2011-10-22 03:09:46 -0700

    More virues

--- visualizer/plugins/botnet/textures.r
+++ visualizer/plugins/botnet/textures.r
@@ -5,3 +5,4 @@ texture wall textures/wall.png
 texture red-server textures/red-server.png
 texture blue-server textures/blue-server.png
 texture red-virus textures/red-virus.png
+texture blue-virus textures/blue-virus.png
