e6d12c964f266cb42af4d1b0de4d5bce267c0b28
Matthew Nuckolls <matthew.nuckolls@gmail.com>
2011-10-29 10:25:30 -0500

    Many changes to the back end, for EC2-ness.
    So that concept of running both clients and a server on a single machine?
    It works really well.

--- arena/thunderdome/blaster.py
+++ arena/thunderdome/blaster.py
@@ -3,8 +3,8 @@
 #####
 
 # Some magic to get a standalone python program hooked in to django
-import sys
-sys.path = ['/srv/', '/srv/uard'] + sys.path
+import sys, os
+sys.path = ['/home/gladiator', '/home/gladiator/djangolol'] + sys.path
 
 from django.core.management import setup_environ
 import settings
@@ -12,51 +12,25 @@ import settings
 setup_environ(settings)
 
 # Non-Django 3rd Party Imports
-import beanstalkc
 import subprocess
 import time
 
-
 def main():
-    stalk = beanstalkc.Connection()
-    stalk.watch('master-requests')
-
-    port = 19000
-    servers = list()
-    for i in xrange(2):
-        servers.append( start_server(i, str(port)) )
-        port += 1
-        
-    refs = list()
-    port = 19000
-    for server_id in xrange(len(servers)):
-        for i in xrange(8):
-            refs.append( start_referee(server_id, i, '/home/mnuck/arena/s%i/server' % server_id, str(port)))
-        port += 1
-
+    start_server()
+    start_referee("%s/server" % os.getcwd())
     while True:
         time.sleep(1)
 
 
-def start_server(server_id, port='19000'):
-    subprocess.call(['rm', '-rf', 's%s' % str(server_id)])
-    subprocess.call(['mkdir', 's%s' % str(server_id)])
-    subprocess.call(['cp', '-R', 'server', 's%s' % str(server_id)])
-    command = ['python', 'main.py', '-arena', '-port', port]
-    return subprocess.Popen(command, cwd='s%s/server' % str(server_id),
+def start_server():
+    command = ['python', 'main.py', '-arena']
+    return subprocess.Popen(command, cwd='server',
                             stdout=file("/dev/null", "w"),
                             stderr=subprocess.STDOUT)
 
 
-def start_referee(server_id, ref_id, server_path, port='19000'):
-    subprocess.call(['rm', '-rf', '%s-%s' % (str(server_id),str(ref_id))])
-    subprocess.call(['mkdir', '%s-%s' % (str(server_id),str(ref_id))])
-    subprocess.call(['ln', '../aws_creds.py'], cwd='%s-%s/' % (str(server_id),str(ref_id)))
-    subprocess.call(['ln', '../referee.py'], cwd='%s-%s/' % (str(server_id),str(ref_id)))
-    command = ['./referee.py', server_path, port]
-    print command
-    return subprocess.Popen(command, cwd='%s-%s' % (str(server_id),str(ref_id)))
-#                            stdout=file("/dev/null", "w"),
-#                            stderr=subprocess.STDOUT)
+def start_referee(server_path):
+    command = ['./referee.py', server_path]
+    return subprocess.Popen(command)
 
 main()
--- arena/thunderdome/load_gladiators.py
+++ arena/thunderdome/load_gladiators.py
@@ -2,54 +2,17 @@
 
 from aws_creds import access_cred, secret_cred
 import boto
-import time
 
-import beanstalkc
-import json
+f = open('user-data', 'r')
+user_data = f.read()
+f.close()
 
-stalk = beanstalkc.Connection()
-
-stalk.watch('free-gladiators')
-stalk.use('free-gladiators')
-
-print "clearing the gladiator pool..."
-while stalk.stats_tube('free-gladiators')['current-jobs-ready'] > 0:
-    j = stalk.reserve()
-    j.delete()
-
-count = 16
+count = 17
 print "spinning up %i gladiators..." % count
 conn = boto.connect_ec2(access_cred, secret_cred)
-gladiator_image = conn.get_image('ami-ef63ac86')
+gladiator_image = conn.get_image('ami-89b779e0')
 reservation = gladiator_image.run(min_count = count, max_count = count,
+                                  user_data = user_data,
                                   instance_type='c1.medium', 
                                   key_name = 'MND_EC2_keypair',
-                                  security_groups = ['MND_SecurityGroup'])
-
-while True:
-    try:
-        states = [x.update() for x in reservation.instances]
-        if all([x == u'running' for x in states]):
-            break
-        print states
-    except boto.exception.EC2ResponseError:
-        pass
-    time.sleep(5)
-    
-gladiator_pairs = list()
-for instance in reservation.instances:
-    pair_thing = [instance.public_dns_name, instance.public_dns_name]
-    gladiator_pairs.append(pair_thing)
-    
-#gladiator_pairs = [ ['localhost', 'localhost'],
-#                    ['localhost', 'localhost'] ]
-
-#print gladiator_pairs
-
-print "loading gladiators into the pool..."
-for pair in gladiator_pairs:
-    s = json.dumps(pair)
-    stalk.put(s)
-
-stalk.close()
-print "done! light the blaster!"
+                                  security_groups = ['MND_SSH'])
